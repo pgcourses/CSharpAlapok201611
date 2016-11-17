fdc270774e381786d10e4118ced4fc3844c7ae19
gplesz <gabor.plesz@gmail.com>
2016-11-17 20:25:23 +0100

    #3, #4, #5, #6: Létrehozó/Véglegesítő Felület, szigorúan tipusos lista

---
+++ Nap2/03FeluletSikidomok/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// System   --------------------------> Linq
+///                       |
+///                       -------------> Text
+///                       |
+///                       -------------> Collection
+///                       |      |
+///                       |      ----------------------> Generic
+///                       |
+///                       -------------> Threading
+///                              |
+///                              ----------------------> Tasks
+///
+
+namespace _03FeluletSikidomok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Letrehozas();
+
+            Console.ReadLine();
+        }
+
+        private static void Letrehozas()
+        {
+            var alap = new Alap("Ez itt a név", "Ez itt a cim");
+            Console.WriteLine();
+
+            var leszarmaztatott = new Leszarmaztatott();
+            Console.WriteLine();
+
+            var tovabbszarmaztatott = new TovabbSzarmaztatott();
+        }
+
+        class Alap
+        {
+            //Amikor példányosítunk egy objektumot, akkor a konstruktora fut le.
+            //
+            //ha nincs ilyen implementálva, akkor a fordító
+            //automatikusan létrehoz egy public paraméter nélküli 
+            //un. alapértelmezett konstruktort
+            
+            //ilyet:
+            //public Alap() { }
+
+            string Nev;
+            string Cim;
+
+
+            public Alap()
+            {
+                Console.WriteLine("Alap konstruktor");
+            }
+
+            //konstruktor overloading
+            public Alap(string nev) : this() //ez pedig a paraméter nélküli konstruktort hívja
+            {
+                Console.WriteLine("Alap konstruktor 2: {0}", nev);
+                Nev = nev;
+            }
+
+            //további konstruktor overloading, ami átadja a már meglévő konstruktornak 
+            //a feladatnak azt a részét amit ő már kezel
+            public Alap(string nev, string cim) : this(nev) //ez meghívja a paraméterrel az előző konstruktort
+            {
+                Console.WriteLine("Alap konstruktor 3: {0}, {1}", nev, cim);
+                Cim = cim;
+            }
+
+            //Finalizer, ami akkor fut, ha az osztálypéldány befejezte életét
+            //Őt magát nem lehet hívni, a futtatókörnyezet hívja meg valamikor
+            //részletesen a Garbage Collector és az IDisposable tartalomnál
+            //megbeszéljük
+            ~Alap()
+            {
+                Console.WriteLine("Alap finalizer");
+            }
+        }
+
+        class Leszarmaztatott : Alap
+        {
+            public Leszarmaztatott()
+            {
+                Console.WriteLine("Leszarmaztatott konstruktor");
+            }
+
+            ~Leszarmaztatott()
+            {
+                Console.WriteLine("Leszármaztatott finalizer");
+            }
+        }
+
+        class TovabbSzarmaztatott : Leszarmaztatott
+        {
+            public TovabbSzarmaztatott()
+            {
+                Console.WriteLine("TovabbSzarmaztatott konstruktor");
+            }
+
+            ~TovabbSzarmaztatott()
+            {
+                Console.WriteLine("TovabbSzarmaztatott finalizer");
+            }
+        }
+    }
+}
---
+++ Nap2/04Sikidomok/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Sikidomok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teglalap = new Teglalap(magassag: 3, szelesseg: 2);
+            var haromszog = new Haromszog(alap: 10, magassag: 5);
+            var kor = new Kor(sugar: 12);
+
+            var lista = new List<ISikidom>();
+
+            lista.Add(teglalap);
+            lista.Add(haromszog);
+            lista.Add(kor);
+
+            var sum = 0;
+            foreach (var sikidom in lista)
+            {
+                sum = sum + sikidom.Terulet();
+            }
+
+            Console.WriteLine("Területek összege: {0}", sum);
+
+            Console.ReadLine();
+        }
+
+        class Teglalap : ISikidom
+        {
+            public Teglalap(int magassag, int szelesseg)
+            {
+                Magassag = magassag;
+                Szelesseg = szelesseg;
+            }
+
+            public int Magassag { get; private set; }
+            public int Szelesseg { get; private set; }
+
+            public int Terulet()
+            {
+                return Magassag * Szelesseg;
+            }
+        }
+
+        class Haromszog : ISikidom
+        {
+            public Haromszog(int alap, int magassag)
+            {
+                Alap = alap;
+                Magassag = magassag;
+            }
+
+            public int Alap { get; private set; }
+            public int Magassag { get; private set; }
+
+            public int Terulet()
+            {
+                return Alap * Magassag;
+            }
+        }
+
+        class Kor : ISikidom
+        {
+            public Kor(int sugar)
+            {
+                Sugar = sugar;
+            }
+
+            public int Sugar { get; private set; }
+
+            public int Terulet()
+            {
+                return (int)(Sugar * Sugar * Math.PI);
+            }
+        }
+
+        /// <summary>
+        /// Közös felület, minden síkidomnak kell, hogy legyen egy terület függvénye
+        /// </summary>
+        interface ISikidom
+        {
+            int Terulet();
+        }
+
+        //Közös ősosztályból nem tudok kiindulni, mert nem tudom megmondani
+        //hogy mennyi a területe
+        //class Sikidom
+        //{
+        //    public int Terulet()
+        //    {
+        //        return 0;
+        //    }
+        //}
+
+    }
+}
