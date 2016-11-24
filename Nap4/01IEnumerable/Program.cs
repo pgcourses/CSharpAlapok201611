a9bae2a67b556ea31ad549232f225ca692d3dc75
gplesz <gabor.plesz@gmail.com>
2016-11-24 17:42:41 +0100

    #9: Első példa: yield return

---
+++ Nap4/01IEnumerable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1 kg  kenyér");
+            //Console.WriteLine("10 dkg párizsi");
+            //Console.WriteLine("1 l tej");
+            //Console.WriteLine("1 kg liszt");
+            //Console.WriteLine("1 még valami");
+            //Console.WriteLine("meg még valami");
+            //Console.WriteLine("és egy kis édesség");
+
+            foreach (var listaElem in BevasarloLista())
+            {
+                Console.WriteLine(listaElem);
+            }
+
+            Console.ReadLine();
+
+            //var list = new List<string>();
+            //var list2 = new List<Sikidom>();
+
+        }
+
+        private static IEnumerable<string> BevasarloLista()
+        {
+            yield return "1 kg  kenyér";
+            yield return "10 dkg párizsi";
+            yield return "1 l tej";
+            yield return "1 kg liszt";
+            yield return "1 még valami";
+            yield return "meg még valami";
+            yield return "és egy kis édesség";
+        }
+
+    }
+}
