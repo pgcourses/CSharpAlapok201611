64fcd3f423f76e539b3fc53470d6bf6f186bbe38
gplesz <gabor.plesz@gmail.com>
2016-11-24 17:53:20 +0100

    #9: első IEnumerable implementáció (nem típusos)

--- Nap4/01IEnumerable/Program.cs
+++ Nap4/01IEnumerable/Program.cs
@@ -19,10 +19,56 @@ namespace _01IEnumerable
             //Console.WriteLine("meg még valami");
             //Console.WriteLine("és egy kis édesség");
 
-            foreach (var listaElem in BevasarloLista())
+            //foreach (var listaElem in BevasarloLista())
+            //{
+            //    Console.WriteLine(listaElem);
+            //}
+
+            var lista = new BejarhatoOsztaly();
+            lista.Add("első");
+            lista.Add("második");
+            lista.Add("harmadik");
+            lista.Add("negyedik");
+            lista.Add("ötödik");
+            lista.Add("hatodik");
+
+            foreach (var elem in lista)
             {
-                Console.WriteLine(listaElem);
+                Console.WriteLine("----foreach elem: {0}", elem);
             }
+
+            //Eredmény
+            //----------------------------------------
+            //    GetEnumerator
+            //    MoveNext(pozicio: 0, vanMeg: True)
+            //    Current(pozicio: 0, elem: első)
+            //----foreach elem: első
+            //    MoveNext(pozicio: 1, vanMeg: True)
+            //    Current(pozicio: 1, elem: második)
+            //----foreach elem: második
+            //    MoveNext(pozicio: 2, vanMeg: True)
+            //    Current(pozicio: 2, elem: harmadik)
+            //----foreach elem: harmadik
+            //    MoveNext(pozicio: 3, vanMeg: True)
+            //    Current(pozicio: 3, elem: negyedik)
+            //----foreach elem: negyedik
+            //    MoveNext(pozicio: 4, vanMeg: True)
+            //    Current(pozicio: 4, elem: ötödik)
+            //----foreach elem: ötödik
+            //    MoveNext(pozicio: 5, vanMeg: True)
+            //    Current(pozicio: 5, elem: hatodik)
+            //----foreach elem: hatodik
+            //    MoveNext(pozicio: 6, vanMeg: False)
+
+            //Ezt a kódot gyártja le a fordító
+            //var bejaro = lista.GetEnumerator();
+            //while (bejaro.MoveNext())
+            //{
+            //    var elem = bejaro.Current;
+            //    Console.WriteLine("----foreach elem: {0}", elem);
+
+            //    //elem feldolgozása
+            //}
 
             Console.ReadLine();
 
@@ -40,6 +86,56 @@ namespace _01IEnumerable
             yield return "1 még valami";
             yield return "meg még valami";
             yield return "és egy kis édesség";
+        }
+
+        class BejarhatoOsztaly : IEnumerable
+        {
+            List<string> lista = new List<string>();
+            public void Add(string elem)
+            {
+                lista.Add(elem);
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                Console.WriteLine("    GetEnumerator");
+                return new Bejaro(lista);
+            }
+        }
+
+        class Bejaro : IEnumerator
+        {
+            private List<string> lista;
+            int pozicio = -1;
+
+            public Bejaro(List<string> lista)
+            {
+                this.lista = lista;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    var current = lista[pozicio];
+                    Console.WriteLine("    Current (pozicio: {0}, elem: {1})", pozicio, current);
+                    return current;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                //pozicio = pozicio + 1;
+                pozicio++;
+                var vanMegElem = pozicio < lista.Count;
+                Console.WriteLine("    MoveNext (pozicio: {0}, vanMeg: {1})", pozicio, vanMegElem);
+                return vanMegElem;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
         }
 
     }
