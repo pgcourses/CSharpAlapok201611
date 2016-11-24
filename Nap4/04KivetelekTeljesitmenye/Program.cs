0b6b11d23f63cb69d554ad7db774e14185e88ff2
gplesz <gabor.plesz@gmail.com>
2016-11-24 19:50:40 +0100

    #10: Exception-ok kezelése

---
+++ Nap4/04KivetelekTeljesitmenye/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04KivetelekTeljesitmenye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception) {  }
+            }
+
+            Console.WriteLine("Eltelt idő: {0}", sw.ElapsedTicks);
+
+            sw.Restart();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                //try
+                //{
+                //    throw new Exception();
+                //}
+                //catch (Exception) { }
+            }
+
+            Console.WriteLine("Eltelt idő: {0}", sw.ElapsedTicks);
+
+            //Eltelt idő: 22877042
+            //Eltelt idő: 9
+
+            Console.ReadLine();
+
+        }
+    }
+}
