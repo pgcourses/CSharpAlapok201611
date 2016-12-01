aa76c4b4383db59399acdcd96246225fe8cc5b28
gplesz <gabor.plesz@gmail.com>
2016-12-01 17:19:58 +0100

    #11: Delegate ami értéket ad vissza +a delegate null vizsgálata

--- Nap5/02Delegate/Program.cs
+++ Nap5/02Delegate/Program.cs
@@ -20,15 +20,90 @@ namespace _02Delegate
         //Ez leírja, hogy nincs visszaadott értéke, és egy stringet vár az a függvénytípus, amit a delegate meghatároz
         delegate void PeldaDelegate(string uzenet);
 
+        delegate int FvDelegatePelda();
+
         static void Main(string[] args)
         {
             //ElsoDelegatePelda();
 
-            MasodikDelegatePelda();
+            //MasodikDelegatePelda();
+
+            //HarmadikDelegatePelda();
+
+            NegyedikDelegatePelda();
+
+
             Console.ReadLine();
 
         }
 
+        /// <summary>
+        /// Bemutatjuk, hogy hogyan kell biztosítani hogy a híváslistát
+        /// csak akkor hívjuk meg, ha van rá feliratkozott metódus.
+        /// Különben elszáll a hívás System.NullReferenceException kivétellel
+        /// </summary>
+        private static void NegyedikDelegatePelda()
+        {
+            PeldaDelegate hivaslista=null;
+
+            //ez helyett:
+            //if (hivaslista != null)
+            //Lemásoljuk a híváslistát egy lokális változóba
+            var lista = hivaslista;
+            //Erre ellenőrzünk
+            if (lista != null)
+            { //majd ha lehet, akkor hívunk híváslistát
+                lista("paraméter");
+            }
+
+            //ha valakinek VS2015 vagy azutáni környezete van, akkor c C# már ilyet is tud:
+            //lista?.Invoke("paraméter");
+
+
+            hivaslista("ez egy példa");
+        }
+
+        /// <summary>
+        /// Ez a példa azt mutatja meg, hogy visszatérési értékkel rendelkező
+        /// függvényeket is feliratkoztathatunk egy ilyen híváslistára. De itt csak az egyik
+        /// visszatérési értékét kapjuk vissza, ami nem garantált, hogy listán az utolsó függvény 
+        /// hívásával képződött eredményt kapjuk
+        /// </summary>
+        private static void HarmadikDelegatePelda()
+        {
+            FvDelegatePelda fuggvenyek;
+            
+            fuggvenyek = delegate { return 0; };
+
+            fuggvenyek += ElsoFuggveny;
+
+            fuggvenyek += MasodikFuggveny;
+
+            //biztosítjuk, hogy legyen valaki a híváslistán
+            var fv = fuggvenyek;
+            if (fv!=null)
+            {
+                var eredmeny = fuggvenyek();
+                Console.WriteLine("Eredmeny {0}", eredmeny);
+            }
+
+        }
+
+        private static int MasodikFuggveny()
+        {
+            Console.WriteLine("2-dik");
+            return 2;
+        }
+
+        private static int ElsoFuggveny()
+        {
+            Console.WriteLine("1-ső");
+            return 1;
+        }
+
+        /// <summary>
+        /// Egy példa arra, hogy egy módosítást is átadhatunk delegate segítségével
+        /// </summary>
         private static void MasodikDelegatePelda()
         {
             var modositoOsztaly = new ModositoOsztaly();
@@ -84,7 +159,13 @@ namespace _02Delegate
             hivasLista += HarmadikFuggveny;
 
             //itt pedig a híváslistát aktivizálom
-            hivasLista("ElsoUzenet");
+
+            //biztosítjuk, hogy legyen valaki a híváslistán
+            var hl = hivasLista;
+            if (hl!=null)
+            {
+                hivasLista("ElsoUzenet");
+            }
         }
 
         private static void HarmadikFuggveny(string uzenet)
@@ -114,7 +195,12 @@ namespace _02Delegate
             for (int i = 0; i < lista.Count; i++)
             {
                 var x = lista[i];
-                fvHivaslista(ref x);
+                //biztosítjuk, hogy legyen valaki a híváslistán
+                var fvhl = fvHivaslista;
+                if (fvhl!=null)
+                {
+                    fvHivaslista(ref x);
+                }
                 lista[i] = x;
             }
         }
