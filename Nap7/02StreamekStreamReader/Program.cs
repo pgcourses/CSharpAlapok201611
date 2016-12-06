dc2c34ba69477588177791c844ea96b6e08992f4
gplesz <gabor.plesz@gmail.com>
2016-12-06 18:17:54 +0100

    #15: XmlSerializer.Serialize/Deserialize, +JSON.Serialize

---
+++ Nap7/02StreamekStreamReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02StreamekStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "teszt.txt";
+            File.WriteAllText(filename,
+                        string.Format("Ez a kiírandó tartalom. \n vagy {0} is írhatok. {0} További speciális karakterek: {1}, {2}, {3}, {4}"
+                            , Environment.NewLine
+                            , (char)113 //ASCII kódból karakter
+                            , Convert.ToChar(115) //ugyanez másként
+                            , '\u0027' //UNICODE karakter írása
+
+                            //készítünk egy byte tömböt karakterkódokkal
+                            //megadjuk az encoding-ot
+                            //majd string-gé alakítjuk
+                            , new string(Encoding.ASCII.GetChars(new byte[] { 35, 36 }))
+                            )
+                            , Encoding.UTF8);
+
+            using (var fs = new FileStream(filename, FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var text = sr.ReadLine();
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Nap7/03SerializeDeserialize/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _03SerializeDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adat = new Adatosztaly()
+            {
+                Egesz = int.MaxValue,
+                Tizedestort = 12.728m,
+                Datum = DateTime.MaxValue,
+                DatumMin = DateTime.MinValue,
+                Szoveg = "ÁrvíztűrőTükörfúrógép"
+            };
+
+            var filenev = "teszt.txt";
+
+            var serializer = new XmlSerializer(typeof(Adatosztaly));
+
+            using (var fs = new FileStream(filenev, FileMode.Create))
+            {
+                serializer.Serialize(fs, adat);
+            }
+
+            using (var fs = new FileStream(filenev, FileMode.Open))
+            {
+                var beolvasott = serializer.Deserialize(fs);
+                Console.WriteLine(JsonConvert.SerializeObject(beolvasott, Formatting.Indented));
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Adatosztaly
+    {
+        public int Egesz { get; set; }
+        public decimal Tizedestort { get; set; }
+        public DateTime Datum { get; set; }
+        public DateTime DatumMin { get; set; }
+        public string Szoveg { get; set; }
+    }
+
+}
