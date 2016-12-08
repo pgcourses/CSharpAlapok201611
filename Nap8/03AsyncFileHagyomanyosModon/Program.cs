1530bd40a4bae1397736e52f142de0e4e3a0751e
gplesz <gabor.plesz@gmail.com>
2016-12-08 18:00:33 +0100

    17: File kezelés aszinkron módon rekurzív függvénnyel lambda helyett.

---
+++ Nap8/03AsyncFileHagyomanyosModon/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03AsyncFileHagyomanyosModon
+{
+    class Program
+    {
+        static int pufferMeret = 90000;
+        static byte[] puffer = new byte[pufferMeret];
+
+        static void Main(string[] args)
+        {
+            string filenev = "teszt.txt";
+            using (var fscreate = new FileStream(filenev, FileMode.Create))
+            {
+                fscreate.SetLength(1000000);
+            }
+
+            var fs = new FileStream(filenev, FileMode.Open);
+            fs.BeginRead(puffer, 0, pufferMeret, CallbackFv, fs);
+
+            //innentől kezdve a beolvasás megy egy másik szálon, nem kell vele foglalkozni.
+
+            Console.ReadLine();
+        }
+
+        private static void CallbackFv(IAsyncResult ar)
+        {
+            var fs = (FileStream)ar.AsyncState;
+            var olvasottByteok = fs.EndRead(ar);
+            if (olvasottByteok > 0)
+            { //még nem végeztünk
+                Console.WriteLine("Belovasott byte-ok: {0}, pozíció: {1}", olvasottByteok, fs.Position);
+                fs.BeginRead(puffer, 0, pufferMeret, CallbackFv, fs);
+            }
+            else
+            { //vége a munkának
+                Console.WriteLine("Az állomány végére értünk");
+                fs.Dispose();
+            }
+        }
+    }
+}
