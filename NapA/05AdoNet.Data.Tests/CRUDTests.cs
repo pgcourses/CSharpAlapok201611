d240dd02050ffd503b95f543a75b375af64fd445
gplesz <gabor.plesz@gmail.com>
2016-12-15 20:24:12 +0100

    #21: AdoNet adatbázis hozzáférés

---
+++ NapA/05AdoNet.Data.Tests/CRUDTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _05AdoNet.Data.Tests
+{
+    [TestClass]
+    public class CRUDTests
+    {
+
+        [TestMethod]
+        public void TeacherList()
+        {
+            //Arrange
+            var db = new DbAccess();
+
+            //Act
+            var teachers = db.GetTeachers();
+
+            //Assert
+            Assert.AreEqual(2, teachers.Count);
+
+        }
+
+
+        [TestMethod]
+        public void TeacherCreate()
+        {
+
+        }
+
+        [TestMethod]
+        public void TeacherRead()
+        {
+        }
+
+        [TestMethod]
+        public void TeacherUpdate()
+        {
+        }
+
+        [TestMethod]
+        public void TeacherDelete()
+        {
+        }
+
+    }
+}
---
+++ NapA/05AdoNet.Data/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace _05AdoNet.Data
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassCode { get; set; }
+    }
+}
