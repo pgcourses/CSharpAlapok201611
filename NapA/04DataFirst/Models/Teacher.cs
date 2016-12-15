93fb67a765c68264e96defa9dd8677015a14e282
gplesz <gabor.plesz@gmail.com>
2016-12-15 19:53:07 +0100

    #22: EntityFramework DbFirst

---
+++ NapA/04DataFirst/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04DataFirst
+{
+    public partial class Teacher
+    {
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", Firstname, Lastname);
+            }
+                
+        }
+    }
+}
---
+++ NapA/04DataFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04DataFirst
+{
+    /// <summary>
+    /// DbFirst tutorial: http://www.entityframeworktutorial.net/entityframework6/introduction.aspx
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SchoolContext();
+            foreach (var teacher in db.Teachers.ToList())
+            {
+                Console.WriteLine("{0}", teacher.FullName);
+                Console.WriteLine(" -> Subject: {0}", teacher.Subject.Name);
+                foreach (var student in teacher.Students.ToList())
+                {
+                    Console.WriteLine(" ---> Student: {0}", student.Name);
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
