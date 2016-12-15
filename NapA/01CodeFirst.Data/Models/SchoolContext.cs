abb168d7e1367b05e4460757f08af55cd388511a
gplesz <gabor.plesz@gmail.com>
2016-12-15 18:09:16 +0100

    #20: EntityFramework connectionStrings

--- NapA/01CodeFirst.Data/Models/SchoolContext.cs
+++ NapA/01CodeFirst.Data/Models/SchoolContext.cs
@@ -9,6 +9,15 @@ namespace _01CodeFirst.Data.Models
 {
     public class SchoolContext : DbContext
     {
+        //Így induláskor keres egy SchoolContext nevű connectionString-et, ha van használja,
+        //ha nincs akkor a SchoolContext az adatbázisnév
+        //public SchoolContext() : base("SchoolContext")
+
+        //Ha így írjuk: name=..., akkor ez a connectionStringet jelenti
+        public SchoolContext() : base("name=SchoolContext")
+        { }
+
+
         public DbSet<Teacher> Teachers { get; set; }
     }
 }
